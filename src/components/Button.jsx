d0b4be38cdde33d1432c3b98ec2d507e500251fe
RizqiAmaliyyah <rizqia439@gmail.com>
2023-12-22 12:23:34 +0700

    hapus kode yang dicomment di komponen button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,3 @@
-// import React from 'react';
-
-// export default function Button({ buttonText, buttonColor, onClick}) {
-//   let className = "";
-//   if (buttonColor === 'green') {
-//     className = 'bg-green-700 hover:bg-green-800 dark:bg-green-600 dark:hover:bg-green-700';
-//   } else if (buttonColor === "red") {
-//     className = 'bg-red-700 hover:bg-red-800 dark:bg-red-600 dark:hover:bg-red-700';
-//   } 
-  
-
-//   return (
-//     <button type="button" className="px-2 py-2 mx-2 my-2   focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={onClick}>{buttonText}</button>
-//   )
-// }
-
 import React from 'react';
 
 export default function Button({ buttonText, buttonColor, onClick}) {
